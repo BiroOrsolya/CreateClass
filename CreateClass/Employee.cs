f5f6a6dbee9bf7b7885d5fb533acb172c7bc3af0
biroorsolya <birorso@gmail.com>
2016-03-31 21:44:53 +0200

    Employee class

---
+++ CreateClass/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Employee : Person
+    {
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        private string profession;
+
+        public string Profession
+        {
+            get { return profession; }
+            set { profession = value; }
+        }
+    }
+}
