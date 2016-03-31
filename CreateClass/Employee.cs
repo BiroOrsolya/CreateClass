e0e90e81a67ce3347ec0f60073215305b05f93eb
biroorsolya <birorso@gmail.com>
2016-03-31 21:47:54 +0200

    Room class added, Employee extended

--- CreateClass/Employee.cs
+++ CreateClass/Employee.cs
@@ -8,6 +8,8 @@ namespace CreateClass
 {
     class Employee : Person
     {
+        public Room Room;
+
         private int salary;
 
         public int Salary
@@ -22,6 +24,23 @@ namespace CreateClass
         {
             get { return profession; }
             set { profession = value; }
+        }
+
+        public Employee()
+        {
+            Room = null;
+        }
+
+        public Employee(string name, DateTime birthDate, int salary, string profession) : base(name, birthDate)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            Room = null;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + String.Format(", salary: {0},  profession: {1}, room: {2}", salary, profession, Room.Number);
         }
     }
 }
